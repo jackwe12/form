cd0e60c8f146f50ab24c3340c5643363bea8c20e
Jack Chang <Jackwe@zhangdeMBP.modem>
2020-03-11 16:46:36 +1100

    add hooks & reusable components

---
+++ src/components/RegisterForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './Form';
+// import registerFormModel from '../models/registrationModel';
+
+function RegisterForm(){
+    return (
+        <div className="">
+            <Form />
+        </div>
+    )
+}
+
+export default RegisterForm;
