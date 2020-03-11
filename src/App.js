251be918f11d7c309956c5005c610a6289c445e9
Jack Chang <Jackwe@zhangdeMBP.modem>
2020-03-11 15:14:17 +1100

    add html

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form>
+      <div>
+        <div>Contact Information</div>
+        <label>First Name</label>
+        <select placeholder="None" >
+          <option> Mr</option>
+          <option> Mrs</option>
+          <option> Miss</option>
+          <option> Dr</option>
+        </select>
+        <input type="text"  placeholder="John" name="firstName required"/>
+        <label>Last Name</label>
+        <input type="text" placeholder="Smith" name="firstName required"/>
+        <label>Account Name</label>
+        <input type="text" placeholder="John's Joinery" name="firstName required"/>
+        <label>Company Name (optional)</label>
+        <input type="text" name="firstName required"/>
+        <label>Phone</label>
+        <input type="text" placeholder="02 123 456 78" name="firstName required"/>
+        <label>Fax (optional)</label>
+        <input type="text" placeholder="John's Joinery" name="firstName required"/>
+        <label>Title (optional)</label>
+        <input type="text" placeholder="Owner" name="firstName required"/>
+        <label>Email</label>
+        <input type="text" placeholder="sample@email.com" name="firstName required"/>
+        <label>Email Opt Out</label>
+        <input type="checkbox" name="firstName required"/>
+      </div>
+      <div>
+      <div>Address Information</div>
+        <label>Steet No. & Street</label>
+        <input type="text" placeholder="1, Elizabeth Street" name="firstName required"/>
+        <label>City</label>
+        <input type="text" placeholder="Sydney" name="firstName required"/>
+        <label>State</label>
+        <select placeholder="New South Wales">
+        <option> New South Wales</option>
+        <option> Victoria</option>
+        <option> Queensland</option>
+        <option> Western Australia</option>
+        <option> South Australia</option>
+        <option> Tasmania</option>        
+        </select>
+        <label>Postcode</label>
+        <input type="text" name="firstName required"/>
+      </div>
+      <div>
+        <div>Description Information</div>
+        <label>Description </label>
+        <textarea type="" name="description required"></textarea>
+      </div>
+    </form>
+
+
+  )
 }
 
 export default App;
