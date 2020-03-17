a9cc4ab528a914fdbaba67f5898f59ab720806e2
Jack Chang <Jackwe@zhangdeMBP.modem>
2020-03-17 22:23:37 +1100

    Updates

---
+++ precache-manifest.7b2b3d895220666fb950cc5c94243fb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb712712bcd8636bb0eed8a181e116b",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "3c4dfcccf227dd97847d",
+    "url": "/form/static/css/main.b5d8a9e6.chunk.css"
+  },
+  {
+    "revision": "77e56c357bdbc64224a7",
+    "url": "/form/static/js/2.cf812a0c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/form/static/js/2.cf812a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c4dfcccf227dd97847d",
+    "url": "/form/static/js/main.259cadb4.chunk.js"
+  },
+  {
+    "revision": "bf9c9d0db2d0d0841025",
+    "url": "/form/static/js/runtime-main.6303fbc7.js"
+  },
+  {
+    "revision": "19c7d1b739625423a5fc50e85a92f236",
+    "url": "/form/static/media/Crunch_Accounting.19c7d1b7.png"
+  },
+  {
+    "revision": "16e7ff65090d580a9c3ac5aa7b1351ae",
+    "url": "/form/static/media/Search.16e7ff65.png"
+  }
+]);
